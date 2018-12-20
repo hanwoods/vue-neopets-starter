1611d9a2a53ba06b1e3abeb3f1bfb95ccae3b653
Hannah Woods <hannahw283@gmail.com>
2018-12-20 14:54:03 -0800

    creates initial project and add in welcome page with standard message

---
+++ neopets-starter/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <WelcomePage header_msg="Welcome to one45's neopets!" sub_msg="Please pick a character to begin"/>
+  </div>
+</template>
+
+<script>
+import WelcomePage from './components/WelcomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WelcomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ neopets-starter/src/components/WelcomePage.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="welcome_page">
+    <h1>{{ header_msg }}</h1>
+    <h2>{{ sub_msg }}</h2>
+    </div>     
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+  props: {
+    header_msg: String,
+    sub_msg: String
+  }
+}
+</script>
