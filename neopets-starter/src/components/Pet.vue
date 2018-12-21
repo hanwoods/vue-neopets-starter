316ebcd109f0ce6eedc82fae6767fcf604e1ce25
Hannah Woods <hannahw283@gmail.com>
2018-12-21 11:01:59 -0800

    fill out the pet component

---
+++ neopets-starter/src/components/Pet.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- notice how we use v-bind here instead of just id="{{file_name}}", mustaches cannot be used instead HTML attributes -->
+    <div v-bind:id="file_name">
+        <img v-bind:alt="file_name" :src="getImgUrl(file_name)">
+    </div>
+</template>
+
+<script>
+    function getImgUrl(file_name) {
+        return '../assets/'+file_name+'.png';
+    }
+    export default {
+        name:"Pet",
+        props: {
+            file_name: String
+        }
+    }
+</script>
