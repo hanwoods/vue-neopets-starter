6833ecdec35ad21a37f1fb7010939ca95c51786a
Hannah Woods <hannah@one45.com>
2019-01-23 13:37:41 -0800

    Complete component to render images

--- neopets-starter/src/components/Pet.vue
+++ neopets-starter/src/components/Pet.vue
@@ -1,14 +1,12 @@
 <template>
-    <!-- notice how we use v-bind here instead of just id="{{file_name}}", mustaches cannot be used instead HTML attributes -->
+    <!-- notice how we use v-bind here instead of just id="{{file_name}}", mustaches cannot be used inside of HTML attributes -->
     <div v-bind:id="file_name">
-        <img v-bind:alt="file_name" :src="getImgUrl(file_name)">
+        <!-- here we have to wrap the image url in a require, beacuse assests are not served in development mode -->
+        <img v-bind:alt="file_name" :src="require('../assets/'+file_name+'.png')">
     </div>
 </template>
 
 <script>
-    function getImgUrl(file_name) {
-        return '../assets/'+file_name+'.png';
-    }
     export default {
         name:"Pet",
         props: {
--- neopets-starter/src/components/WelcomePage.vue
+++ neopets-starter/src/components/WelcomePage.vue
@@ -2,15 +2,21 @@
    <div class="welcome_page">
     <h1>{{ header_msg }}</h1>
     <h2>{{ sub_msg }}</h2>
+    <Pet file_name="jubjub_pet"></Pet>
     </div>     
 </template>
 
 <script>
+import Pet from './Pet.vue'
+
 export default {
   name: 'WelcomePage',
   props: {
     header_msg: String,
     sub_msg: String
+  },
+  components: {
+    Pet
   }
 }
 </script>
